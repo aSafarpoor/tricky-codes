a3e98ed627c4f30dc33c69a56b1aaf0190bc4239
ali safarpoor dehkordi <37835450+aSafarpoor@users.noreply.github.com>
2022-10-05 19:43:09 +0330

    Update remove page from pdf with python.py

--- remove page from pdf with python.py
+++ remove page from pdf with python.py
@@ -1,39 +1,15 @@
-# from PyPDF2 import PdfFileMerger, PdfFileReader
-
-# filename1 = 'nlp-lec-17.pdf' 
-# filename2 = 'nlp-lec-18.pdf'
-
-# merger = PdfFileMerger()
-
-# merger.append(PdfFileReader(open(filename1, 'rb')))
-# merger.append(PdfFileReader(open(filename2, 'rb')))
-
-
-
-
-# merger.write("merged.pdf")
-
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
-#page number is start from 0 :)
-ptk1 = list(range(2,21))
-ptk2 = list(range(19,29))
+ptk1 = list(range(1,11))
 
-
-filename1 = 'nlp-lec-17.pdf' 
-filename2 = 'nlp-lec-18.pdf'
+filename1 = 'Are Graph Augmentations Necessary Simple Graph Contrastive Learning for Recommendation.pdf' 
 
 infile1 = PdfFileReader(filename1, 'rb')
-infile2 = PdfFileReader(filename2, 'rb')
 
 output = PdfFileWriter()
 
 for i in ptk1:
     p = infile1.getPage(i)
-    output.addPage(p)
-
-for i in ptk2:
-    p = infile2.getPage(i)
     output.addPage(p)
 
 with open('newfile.pdf', 'wb') as f:
