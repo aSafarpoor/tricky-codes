28f283a6afd875084bc1bf6521791f9d8e198c37
ali safarpoor dehkordi <37835450+aSafarpoor@users.noreply.github.com>
2022-10-18 19:38:46 +0330

    Create pickle-related.py

---
+++ pickle-related.py
@@ -0,0 +1,9 @@
+import pickle
+
+a = {'hello': 'world'}
+
+with open('filename.pickle', 'wb') as handle:
+    pickle.dump(a, handle)
+
+with open('filename.pickle', 'rb') as handle:
+    b = pickle.load(handle)
